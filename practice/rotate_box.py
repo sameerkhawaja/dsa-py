bfc92f2853ab306e1894cdc9860caa781e8bf1bb
Sameer Khawaja <sameerkhawaja01@gmail.com>
2024-01-13 17:12:56 -0500

    add rotate box problem

---
+++ practice/rotate_box.py
@@ -0,0 +1,15 @@
+def rotateBox(box):
+    res = []
+    num_rows = len(box)
+    num_col = len(box[0])
+
+    for col in range(num_col):
+        new_row = []
+        for row in range(num_rows):
+            new_row.append(box[row][col])
+        res.append(new_row)
+    return res
+
+arr=[[-1,-2,-3,-4],[1,2,3,4]]
+res = rotateBox(arr)
+print(res)
